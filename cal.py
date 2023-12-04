a37d7f386b58b40e6b7aacee38a164528dd1aedd
Diksha Sharma <dikshasharma1725@gmail.com>
2023-12-04 18:51:53 +0530

    Calculater

---
+++ cal.py
@@ -0,0 +1,19 @@
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+sum = a+b
+print("the value of", a, "+", b, ":", sum)
+
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+subtract = a-b
+print("the value of", a, "-", b, ":", subtract)
+
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+multiply = a*b
+print("the value of", a, "*", b, ":", multiply)
+
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
+divide = a/b
+print("the value of", a, "/", b, ":", divide)
